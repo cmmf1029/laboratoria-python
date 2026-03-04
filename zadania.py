484f47964167904d1cd0e171f95be6c27fded18e
Magda <126404019+cmmf1029@users.noreply.github.com>
2026-03-04 18:38:16 +0100

    Add files via upload

---
+++ zadania.py
@@ -0,0 +1,55 @@
+#zadanie 1
+# print(0 > 1)
+# print(0 <= 1)
+# print(0 >= 1)
+# print(1 == 0)
+# print(1 == 1)
+# print(1 != 0)
+# print(1 != 1)
+
+
+#zadanie 2
+
+# x = float(input('Podaj x: '))
+# y = float(input('Podaj y: '))
+# wynik = 2*x+5*y
+# print(wynik)
+
+#zadanie 3
+# a=str(input("Podaj imie: "))
+# b=str(input("Podaj nazwisko: "))
+# c=int(input("Podaj wiek: "))
+# d=str(input("Co studiujesz?: "))
+# print("Jestem ",a, b," mam ",c, "lat, studiuję ",d)
+
+#zadanie 4
+# a=1+2+10+20000001+4+347586970885
+# print(a==321784560456434534646)
+
+#zadanie 5
+# liczba1 =int(input("Podaj liczbe: "))
+# liczba2=int(input("Podaj liczbe: "))
+# suma=liczba1+liczba2
+# if suma%2==0:
+#     print("Suma jest parzysta ")
+# else:
+#     print("Suma jest nieparzysta")
+
+#zadanie 6
+# l1=float(input("Podaj liczbe: "))
+# l2=float(input("Podaj liczbe: "))
+# print("Dodawanie: ", l1+l2)
+# print("Odejmowanie: ", l1-l2)
+# print("Mnożenie: ", l1*l2)
+# print("Potęgowanie: ", l1**l2)
+# if l2!=0:
+#     print("Dzielenie: ", l1/l2)
+# else:
+#     print("Nie można dzielić przez 0")
+
+
+#zadanie 7
+# x=float(input("Podaj x: "))
+# print((x > 1 and x < 13))
+# print((x != 5 or x < 0))
+
